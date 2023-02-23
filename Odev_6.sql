3f67c7baa930a01ad024b18bf926ecb6231c6ec0
orkunumur <orkunumur@gmail.com>
2023-02-23 14:17:30 +0300

    Ödev 6

---
+++ Odev_6.sql
@@ -0,0 +1,16 @@
+--................................ ÖDEV 4 ....................................
+
+-- SORU 1: film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+SELECT ROUND(AVG(rental_rate),3) FROM film; -- 2.980
+
+-- SORU 2: film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
+SELECT COUNT(title) FROM film
+WHERE title LIKE 'C%'; -- 92
+
+-- SORU 3: film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+SELECT MAX(length) FROM film
+WHERE rental_rate = 0.99; --184
+
+-- SORU 4: film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+SELECT COUNT(DISTINCT replacement_cost), length FROM film
+WHERE length > 150; -- 21
