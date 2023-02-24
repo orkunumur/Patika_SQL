12299ff725d93c3fd195d6665fa8655d5abc085a
orkunumur <orkunumur@gmail.com>
2023-02-24 13:14:09 +0300

    ödev 7

---
+++ Odev_7.sql
@@ -0,0 +1,25 @@
+--................................ ÖDEV 7 ....................................
+
+-- SORU 1: film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+SELECT rating, COUNT(*) FROM film
+GROUP BY  rating;
+
+
+-- SORU 2: film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla 
+-- olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+SELECT replacement_cost, COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*) > 50;
+
+
+-- SORU 3: customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir? 
+SELECT store_id, COUNT(*) FROM customer
+GROUP BY store_id;
+
+
+-- SORU 4: city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı 
+-- barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+SELECT country_id, COUNT(city) FROM city
+GROUP BY country_id
+ORDER BY COUNT(city) DESC
+LIMIT 1;
